ed2ebcddcbddab05900b4e92b5510af764e142d3
Chris Schlensker <chris@foxyblocks.com>
2023-08-07 16:50:57 -0700

    Add supabase to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   // Create authenticated Supabase Client.
+  const supabase = createMiddlewareClient({ req, res });
   // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // Check auth condition
+  if (session?.user || req.nextUrl.searchParams.has('login')) {
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
 
   if (req.nextUrl.pathname.startsWith('/api')) {
     return res;
@@ -16,7 +27,7 @@ export async function middleware(req: NextRequest) {
 
   // Auth condition not met, redirect to home page.
   const redirectUrl = req.nextUrl.clone();
-  redirectUrl.pathname = '/javascript/dashboard/filter/recent';
+  redirectUrl.pathname = '/feed';
   redirectUrl.searchParams.set('redirectedFrom', req.nextUrl.pathname);
   return NextResponse.redirect(redirectUrl);
 }
